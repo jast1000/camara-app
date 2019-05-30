1b6bdb704df5be4fd8e1667dd2dd7bf18fe4308a
jesussanchez@motumweb.com <GI050893?>
2019-05-30 17:52:49 -0500

    Primer commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import { BarcodeScanResult, BarcodeScanner} from '@ionic-native/barcode-scanner/ngx'
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,37 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  n1: number;
+  n2: number;
+  photo: string;
+  qr: string;
+  constructor(
+    private camera: Camera,
+    private barcoscanner: BarcodeScanner
+  ) {}
 
-  constructor() {}
+  async readQR(){
+    let result: BarcodeScanResult = await this.barcoscanner.scan();
+    this.qr = result.text;
+  }
 
+  async takePhoto() {
+    const options: CameraOptions = {
+      quality: 50,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    };
+    let img: string = await this.camera.getPicture(options); 
+    this.photo = "data:image/jpeg;base64," + img;
+  }
+
+  sumar(){
+    let r:number= (+(this.n1)) + (+(this.n2));
+    console.log(r);
+  }
+  restar(){
+    let r:number= (+(this.n1)) - (+(this.n2));
+    console.log(r);
+  }
 }
